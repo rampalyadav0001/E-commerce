a4c13b5ad2ecddc4d9d0e590d64c3ed3de9aad22
rampal <rampalyadav23082001@gmail.com>
2023-09-10 10:03:31 +0530

    Updates

---
+++ precache-manifest.a48ab47540c5b3100503d7c46db3bf59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd831b08e13802bc8afdfb2029229c3",
+    "url": "/E-commerce/index.html"
+  },
+  {
+    "revision": "2554cd00e69fdcaad532",
+    "url": "/E-commerce/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "96246ab7e2a249387188",
+    "url": "/E-commerce/static/css/main.25ae3c87.chunk.css"
+  },
+  {
+    "revision": "2554cd00e69fdcaad532",
+    "url": "/E-commerce/static/js/2.e15c42af.chunk.js"
+  },
+  {
+    "revision": "96246ab7e2a249387188",
+    "url": "/E-commerce/static/js/main.e4e07223.chunk.js"
+  },
+  {
+    "revision": "3c3848a61e768f552efe",
+    "url": "/E-commerce/static/js/runtime~main.7474c067.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/E-commerce/static/media/logo.9f7e506d.svg"
+  }
+]);
